ab5482c09e56425970da643edcbc311ba168ffad
Vihar8 <168709645+Vihar8@users.noreply.github.com>
2024-12-16 19:32:37 +0530

    title and buyer

--- F2Ffront/src/pages/Buyesrs.jsx
+++ F2Ffront/src/pages/Buyesrs.jsx
@@ -29,25 +29,30 @@ const BuyerCommodities = () => {
 
   return (
     <div>
-      <h1>Buyer Commodities</h1>
-      {commodities.map((item) => (
-        <div key={item._id} style={{ border: '1px solid #ccc', margin: '10px', padding: '10px' }}>
-          <h3>{item.commodity}</h3>
-          <p>Variety: {item.varietyType}</p>
+      <h1 className='text-3xl text-greenCustom text-center font-bold m-2'>Buyer Commodities</h1>
+     <div className='container mx-auto grid grid-cols-1 md:grid-cols-3 gap-5'>
+     {commodities.map((item) => (
+        <div className='rounded-xl' key={item._id} style={{ border: '1px solid #ccc', margin: '10px', padding: '10px' }}>
+          <h3 className='text-xl font-mono capitalize'>{item.commodity}</h3>
+          <p className='text-base'>Variety: {item.varietyType}</p>
           <p>Price: {item.price}</p>
           <p>Quantity: {item.quantity} {item.totalIn}</p>
           <p>Location: {item.state}, {item.district}</p>
+          <div className='flex'>
           {item.images.map((img, idx) => (
             <img 
+              className='rounded-lg '
               key={idx} 
               src={`${import.meta.env.VITE_API_URL}/${img}`} 
               alt={item.commodity} 
               style={{ width: '100px', height: '100px', margin: '5px' }} 
             />
           ))}
-          <p>Buyer: {item.buyer.name} ({item.buyer.email})</p>
+          </div>     
+          <p><span className='text-xl'></span> {item.buyer.name} ({item.buyer.email})</p>
         </div>
       ))}
+     </div>
     </div>
   );
 };
