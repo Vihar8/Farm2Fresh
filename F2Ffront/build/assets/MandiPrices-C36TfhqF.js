7790da5e5932152b995c83dd8e5952fec8a1ed41
Vihar8 <168709645+Vihar8@users.noreply.github.com>
2024-12-27 16:50:56 +0530

    final

---
+++ F2Ffront/build/assets/MandiPrices-C36TfhqF.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-BzkZyVbb.js";const s=()=>r.jsx("div",{children:"MandiPrices"});export{s as default};
---
+++ F2Ffront/build/assets/circle-check-big-CNA7E4-M.js
@@ -0,0 +1,26 @@
+import{r as n}from"./index-BzkZyVbb.js";/**
+ * @license lucide-react v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const p=r=>r.replace(/([a-z0-9])([A-Z])/g,"$1-$2").toLowerCase(),c=(...r)=>r.filter((e,t,o)=>!!e&&e.trim()!==""&&o.indexOf(e)===t).join(" ").trim();/**
+ * @license lucide-react v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var C={xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:2,strokeLinecap:"round",strokeLinejoin:"round"};/**
+ * @license lucide-react v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const f=n.forwardRef(({color:r="currentColor",size:e=24,strokeWidth:t=2,absoluteStrokeWidth:o,className:s="",children:a,iconNode:i,...l},m)=>n.createElement("svg",{ref:m,...C,width:e,height:e,stroke:r,strokeWidth:o?Number(t)*24/Number(e):t,className:c("lucide",s),...l},[...i.map(([u,d])=>n.createElement(u,d)),...Array.isArray(a)?a:[a]]));/**
+ * @license lucide-react v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const h=(r,e)=>{const t=n.forwardRef(({className:o,...s},a)=>n.createElement(f,{ref:a,iconNode:e,className:c(`lucide-${p(r)}`,o),...s}));return t.displayName=`${r}`,t};/**
+ * @license lucide-react v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const g=h("CircleCheckBig",[["path",{d:"M21.801 10A10 10 0 1 1 17 3.335",key:"yps3ct"}],["path",{d:"m9 11 3 3L22 4",key:"1pflzl"}]]);export{g as C,h as c};
